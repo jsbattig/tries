31a6b333b32c71c95caeb2e3dbca6ce882545c60
Jose Sebastian Battig <jsbattig@convey.com>
2016-01-04 22:45:00 -0600

    B-0 added tests for iterator of IntegerHashTrie

--- test_integerhashtrie.pas
+++ test_integerhashtrie.pas
@@ -26,6 +26,9 @@ type
     procedure TestAddFindAndRemove16;
     procedure TestAddFindAndRemove64;
     procedure TestCreateIntegerHashTrie;
+    procedure TestIterate32;
+    procedure TestIterate16;
+    procedure TestIterate64;
   end;
 
 implementation
@@ -85,6 +88,66 @@ begin
   Check(Value = Pointer(Self), 'Value should be equals to Self');
   Check(FIntHashTrie.Remove(Key), 'Key was not found?');
   Check(not FIntHashTrie.Find(Key, Value), 'Find should return False');
+end;
+
+procedure TIntegerHashTrieTest.TestIterate32;
+var
+  Key : Cardinal;
+  Key32 : Word;
+  Value : pointer;
+  It : THashTrieIterator;
+begin
+  FIntHashTrie := TIntegerHashTrie.Create(hs32);
+  Key := 1;
+  FIntHashTrie.Add(Key, Self);
+  FIntHashTrie.InitIterator(It);
+  Check(FIntHashTrie.Next(It, Key, Value), 'First call to Next should return True');
+  CheckEquals(1, Key, 'Key returned from Next should be  = 1');
+  Check(Value = Pointer(Self), 'Value returned from Next should be Self');
+  Check(not FIntHashTrie.Next(It, Key, Value), 'Second call to Next should return False');
+  FIntHashTrie.InitIterator(It);
+  ExpectedException := EIntegerHashTrie;
+  FIntHashTrie.Next(It, Key32, Value);
+end;
+
+procedure TIntegerHashTrieTest.TestIterate16;
+var
+  Key : Word;
+  Key32 : Cardinal;
+  Value : pointer;
+  It : THashTrieIterator;
+begin
+  FIntHashTrie := TIntegerHashTrie.Create(hs16);
+  Key := 1;
+  FIntHashTrie.Add(Key, Self);
+  FIntHashTrie.InitIterator(It);
+  Check(FIntHashTrie.Next(It, Key, Value), 'First call to Next should return True');
+  CheckEquals(1, Key, 'Key returned from Next should be  = 1');
+  Check(Value = Pointer(Self), 'Value returned from Next should be Self');
+  Check(not FIntHashTrie.Next(It, Key, Value), 'Second call to Next should return False');
+  FIntHashTrie.InitIterator(It);
+  ExpectedException := EIntegerHashTrie;
+  FIntHashTrie.Next(It, Key32, Value);
+end;
+
+procedure TIntegerHashTrieTest.TestIterate64;
+var
+  Key : Int64;
+  Key32 : Cardinal;
+  Value : pointer;
+  It : THashTrieIterator;
+begin
+  FIntHashTrie := TIntegerHashTrie.Create(hs64);
+  Key := 1;
+  FIntHashTrie.Add(Key, Self);
+  FIntHashTrie.InitIterator(It);
+  Check(FIntHashTrie.Next(It, Key, Value), 'First call to Next should return True');
+  CheckEquals(1, Key, 'Key returned from Next should be  = 1');
+  Check(Value = Pointer(Self), 'Value returned from Next should be Self');
+  Check(not FIntHashTrie.Next(It, Key, Value), 'Second call to Next should return False');
+  FIntHashTrie.InitIterator(It);
+  ExpectedException := EIntegerHashTrie;
+  FIntHashTrie.Next(It, Key32, Value);
 end;
 
 initialization
